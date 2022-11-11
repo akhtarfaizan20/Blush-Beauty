475fdc7d2720774a041a9a4ea91b717ab7e66e3d
akhtarfaizan20 <akhtarfaizan20@gmail.com>
2022-11-11 17:35:19 +0530

    cart functionility done

--- cartpage.js
+++ cartpage.js
@@ -1,12 +1,23 @@
-console.log("tocheck")
+console.log("tocheck");
 
-function giftcode(){
-    let giftcode = document.getElementById("giftcode").value;
-    let giftcodeL = giftcode.length
+function giftcode() {
+  let giftcode = document.getElementById("giftcode").value;
+  let giftcodeL = giftcode.length;
 
-   if(giftcodeL>2){
-   let  applybtn= document.getElementById("applybtn")
-      applybtn.style ="background-color:black"
-   }
+  if (giftcodeL > 2) {
+    let applybtn = document.getElementById("applybtn");
+    applybtn.style = "background-color:black";
+  }
 }
+let logginedUser = JSON.parse(localStorage.getItem("loginneduser"));
+// console.log(logginedUser);
+window.onload = async () => {
+  let response = await fetch(`http://localhost:3000/cart`);
+  let data = await response.json();
 
+  let actualdata = data.filter((el) => {
+    return el.user_id == logginedUser.id;
+  });
+
+  console.log(actualdata);
+};
